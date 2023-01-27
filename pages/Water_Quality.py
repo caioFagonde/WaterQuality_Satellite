ec7ac8ef0db1d09dbc9e55ff08dfdafac81bc791
Caio Fagonde <caio.fagonde@aluno.ufabc.edu.br>
2023-01-27 11:55:01 -0300

    z

--- pages/Water_Quality.py
+++ pages/Water_Quality.py
@@ -463,8 +463,9 @@ for i in range(0,len(np.array(onlyfiles))):
 
 def PlotFrom2DF(df1,df2, title, field1,field2):
     
-    df1["smooth"] = np.polyval(np.polyfit(df1["dt"].astype(int),df1[field1], 5), df1["dt"].astype(int))
-    df2["smooth"] = np.polyval(np.polyfit(df2["Aux_date"].astype(int),df2[field2], 5), df1["dt"].astype(int))
+    df1["smooth"] = np.polyval(np.polyfit(df1["dt"].astype(float),df1[field1], 5), df1["dt"].astype(int))
+    df2["dt"] = df1["dt"]
+    df2["smooth"] = np.polyval(np.polyfit(df2["Aux_date"].astype(float),df2[field2], 5), df2["dt"].astype(float))
     
     fig, axes = plt.subplots(figsize = (20,5))
     axes.set_title(title)
