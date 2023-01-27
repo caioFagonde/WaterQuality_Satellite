f301b1576c17f13fc3f97da5b109958e73771fb7
Caio Fagonde <caio.fagonde@aluno.ufabc.edu.br>
2023-01-27 10:16:53 -0300

    a

--- pages/Water_Quality.py
+++ pages/Water_Quality.py
@@ -407,7 +407,7 @@ def generate_tif(depth = 4, filename = ""):
     dissolved_oxygen[ndwi < -0.4] = np.nan
     bathymetry[ndwi < -0.4] = np.nan
     bathymetry[bathymetry < 0] = np.nan
-    bathymetry[bathymetry < 10] = np.nan
+    bathymetry[bathymetry > 20] = np.nan
     # check range values, excluding NaN
     #np.nanmin(dissolved_oxygen), np.nanmax(dissolved_oxygen)
     #np.nanmin(dissolved_oxygen), np.nanmax(dissolved_oxygen)
