1922add9390e7e5c294bf3f6b93cd39e50032432
Caio Fagonde <caio.fagonde@aluno.ufabc.edu.br>
2023-02-03 10:20:50 -0300

    a

--- pages/Water_Quality.py
+++ pages/Water_Quality.py
@@ -163,7 +163,7 @@ def generate_tif(depth = 4, filename = "",img = ""):
     lrrs_vecblue = np.log(rrs_vecblue)
     lrrs_vecgreen = np.log(rrs_vecgreen)
     
-    chla = np.power(10,-0.4909 + 191.659*w)#0.5
+    chla = 0.5#np.power(10,-0.4909 + 191.659*w)#0.5
     m0 = 52.073*np.exp(0.957*chla)
     m1 = 50.156*np.exp(0.957*chla)
     
